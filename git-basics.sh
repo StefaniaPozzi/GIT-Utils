74469aed373b0fd5637bfabafbd1dfe9d715cbdd
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-02-01 10:25:11 +0100

    Add hotfix

--- git-basics.sh
+++ git-basics.sh
@@ -6,17 +6,18 @@ git init
 #add file to a new branch
 git branch -f feature/hotfix
 git checkout feature/hotfix
-
 touch hotFix.css
 
+#commit and push changes
 git add --all
 git commit -m "Add hotfix"
+git push --set-upstream origin master
 
 #merge hotfix into master and remove old branch
-git checkout master
-git merge feature/hotfix 
-git branch -d feature/hotfix
-rm hotFix.css
+#git checkout master
+#git merge feature/hotfix 
+#git branch -d feature/hotfix
+#rm hotFix.css
 #handle conflicts.. TODO
 
 #work with Github
