5ba04cfd4ecb51a5b909215bd7bd470e37e8b369
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-02-01 10:25:31 +0100

    Add hotfix

--- git-basics.sh
+++ git-basics.sh
@@ -11,7 +11,7 @@ touch hotFix.css
 #commit and push changes
 git add --all
 git commit -m "Add hotfix"
-git push --set-upstream origin master
+git push --set-upstream origin feature/hotfix
 
 #merge hotfix into master and remove old branch
 #git checkout master
