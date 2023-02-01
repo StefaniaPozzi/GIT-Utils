a5ca173f0050b195a242849f0dd8647a20e0a775
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-02-01 16:33:25 +0100

    inizializing

--- git-basics.sh
+++ git-basics.sh
@@ -7,4 +7,4 @@ NEW_BRANCH_FILE="superHotFix.css"
 NEW_BRANCH_MSG="created new branch"
 
 initialize $INIT_MSG
-#create_new_branch_and_add_new_file $NEW_BRANCH_NAME $NEW_BRANCH_FILE $NEW_BRANCH_MSG
+create_new_branch_and_add_new_file $NEW_BRANCH_NAME $NEW_BRANCH_FILE $NEW_BRANCH_MSG
